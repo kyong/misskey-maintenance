c557f5395c5bcafb7939f070545269d154dc646b
Kyoji Katahira <kyoji.katahira@gmail.com>
2023-10-16 14:34:03 +0900

    Fix param restore

--- restore_backup.py
+++ restore_backup.py
@@ -1,6 +1,14 @@
 import os
 import subprocess
 from dotenv import load_dotenv
+import time 
+import argparse
+
+# 引数の解析
+parser = argparse.ArgumentParser(description='Restore backup for PostgreSQL in Docker.')
+parser.add_argument('source', choices=['S3', 'local'], help='Backup source: S3 or local')
+parser.add_argument('filename', help='Name of the backup file.')
+args = parser.parse_args()
 
 # .envから環境変数を読み込む
 load_dotenv()
@@ -17,12 +25,19 @@ AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 
-# 一時的なコンテナを作成
-# subprocess.run(
-#     f"docker run  --name {TEMP_CONTAINER_NAME} -v {DATABASE_VOLUME}:/var/lib/postgresql/data -v {BACKUP_DIR}:/backup -d {POSTGRES_IMAGE}",
-#     shell=True)
-# print(BACKUP_DIR)
+def check_postgres_ready():
+    cmd = [
+        "docker", "exec", TEMP_CONTAINER_NAME,
+        "psql", "-U", DB_USER, "-d", DB_NAME, "-c", "SELECT 1;"
+    ]
+    try:
+        subprocess.check_output(cmd)
+        return True
+    except subprocess.CalledProcessError:
+        return False
 
+
+# 一時的なコンテナを作成
 container_id = subprocess.check_output([
         "docker", "run", "-d",
         "-e", f"POSTGRES_PASSWORD={DB_PASSWORD}",
@@ -33,22 +48,39 @@ container_id = subprocess.check_output([
         POSTGRES_IMAGE
     ]).decode().strip()
 
-# リストア操作
-backup_source = input("Choose backup source (S3/local): ")
+# PostgreSQLが起動するまで待つ
+while not check_postgres_ready():
+    time.sleep(5)
 
-if backup_source == "local":
-    local_backup_file = input("Enter the backup file name you want to restore (e.g., backup_2023-09-24.dump): ")
+
+# データベースをクリーンな状態にする
+drop_db_command = [
+    "docker", "exec", TEMP_CONTAINER_NAME,
+    "psql", "-U", DB_USER, "-d", "postgres", "-c",
+    f"DROP DATABASE IF EXISTS {DB_NAME};"
+]
+subprocess.run(drop_db_command)
+
+create_db_command = [
+    "docker", "exec", TEMP_CONTAINER_NAME,
+    "psql", "-U", DB_USER, "-d", "postgres", "-c",
+    f"CREATE DATABASE {DB_NAME};"
+]
+subprocess.run(create_db_command)
+
+
+
+if args.source == "local":
     restore_command = [
         "docker", "exec", TEMP_CONTAINER_NAME,
-        "pg_restore", "-U", DB_USER, "-d", DB_NAME, f"/backup/{local_backup_file}"
+        "pg_restore", "-U", DB_USER, "-d", DB_NAME, f"/backup/{args.filename}"
     ]
     subprocess.run(restore_command)
 
-elif backup_source == "S3":
-    s3_backup_file = input("Enter the backup file name from S3 you want to restore (e.g., s3://your_bucket/backup_2023-09-24.dump): ")
+elif args.source == "S3":
     subprocess.run([
         "docker", "exec", TEMP_CONTAINER_NAME,
-        "aws", "s3", "cp", s3_backup_file, "/backup/s3_backup.dump",
+        "aws", "s3", "cp", args.filename, "/backup/s3_backup.dump",
         "--region", "your_region",  # 必要に応じてAWSのリージョンを設定してください。
         "--access-key", AWS_ACCESS_KEY_ID,
         "--secret-access-key", AWS_SECRET_ACCESS_KEY
@@ -59,5 +91,5 @@ elif backup_source == "S3":
     ])
 
 # 一時的なコンテナを停止・削除
-# subprocess.run(f"docker stop {TEMP_CONTAINER_NAME}", shell=True)
-# subprocess.run(f"docker rm {TEMP_CONTAINER_NAME}", shell=True)
+subprocess.run(f"docker stop {TEMP_CONTAINER_NAME}", shell=True)
+subprocess.run(f"docker rm {TEMP_CONTAINER_NAME}", shell=True)
